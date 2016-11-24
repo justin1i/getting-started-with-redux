9e07ee15fe13804e698fcef37029dae13862da7a
justin1i <justin1i@126.com>
2016-11-24 10:14:41 +0800

    refactor the entry point

--- src/index.js
+++ src/index.js
@@ -2,36 +2,9 @@ import React from 'react';
 import {
 	render
 } from 'react-dom';
-import {
-	Provider
-} from 'react-redux';
-import {
-	createStore
-} from 'redux';
-
 import util from './util';
+import Root from './components/Root';
+import configureStore from './configureStore';
+const store = configureStore();
 
-import App from './components/App';
-import todoApp from './reducers';
-
-import {
-	loadState,
-	saveState
-} from './localStorage';
-
-const persistedState = loadState();
-
-const store = createStore(todoApp, persistedState);
-
-store.subscribe(() => {
-	saveState({
-		todos: store.getState().todos
-	});
-});
-
-render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	util.addNode('root')
-);
+render(<Root store={store} />, util.addNode('root'));
--- src/localStorage.js
+++ src/localStorage.js
@@ -11,7 +11,6 @@ export const loadState = () => {
 };
 
 export const saveState = (state) => {
-	console.log("???")
 	try {
 		const serializedState = JSON.stringify(state);
 		localStorage.setItem('state', serializedState);
