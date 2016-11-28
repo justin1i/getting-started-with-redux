da58d21de39ea24eb1725dcff04609ebda547b2c
justin1i <justin1i@126.com>
2016-11-28 15:48:52 +0800

    done

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,7 +3,6 @@ import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-
 const configureStore = () => {
   const middlewares = [thunk];
   if (process.env.NODE_ENV !== 'production') {
